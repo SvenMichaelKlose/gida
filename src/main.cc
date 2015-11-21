938543e7fe065bd711517a5b2591918e7d4c2814
Sven Michael Klose <pixel@copei.de>
2015-11-21 19:16:46 +0100

    Exit with error code after printing help text.

--- src/main.cc
+++ src/main.cc
@@ -17,68 +17,68 @@
 
 void print_version ()
 {
-   cout << PACKAGE << " interactive disassembler version " << VERSION << endl;
+    cout << PACKAGE << " interactive disassembler version " << VERSION << endl;
 }
 
 void print_help ()
 {
-   print_version ();
-   cout << "Usage: " << PACKAGE << " [-6502|-6802] MYROM.BIN <MYROM.CONF" << endl;
-   exit (0);
+    print_version ();
+    cout << "Usage: " << PACKAGE << " [-6502|-6802] MYROM.BIN <MYROM.CONF" << endl;
+    exit (-1);
 }
 
 int main (int argc, char** argv)
 {
-   bool no_trace = false;
-   bool no_unreached_code = false;
-   vcpu* cpu = NULL;
-   vaddr offset = 0x8000;
-   const char * pathname_in = NULL;
+    bool no_trace = false;
+    bool no_unreached_code = false;
+    vcpu* cpu = NULL;
+    vaddr offset = 0x8000;
+    const char * pathname_in = NULL;
 
-   if (argc == 1)
-      print_help ();
+    if (argc == 1)
+        print_help ();
 
-   int a;
-   for (a = 1; a < argc; a++) {
-       if (!strcmp (argv[a], "-6502"))
-          cpu = cpu_6502::instance ();
-       else if (!strcmp (argv[a], "-6800"))
-          cpu = cpu_6800::instance ();
-       else
-          break;
-   }
-   if (!cpu) {
-       cerr << "No CPU type specified." << endl;
-       print_help ();
-   }
-   if (a < argc -1) {
-       cerr << "Too many arguments." << endl;
-       print_help ();
-   }
-   pathname_in = argv[a];
+    int a;
+    for (a = 1; a < argc; a++) {
+         if (!strcmp (argv[a], "-6502"))
+             cpu = cpu_6502::instance ();
+         else if (!strcmp (argv[a], "-6800"))
+             cpu = cpu_6800::instance ();
+         else
+             break;
+    }
+    if (!cpu) {
+        cerr << "No CPU type specified." << endl;
+        print_help ();
+    }
+    if (a < argc -1) {
+        cerr << "Too many arguments." << endl;
+        print_help ();
+    }
+    pathname_in = argv[a];
 
-   fstream infile;
-   infile.open (pathname_in, ios::in | ios::binary);
-   if (!infile.is_open ()) {
+    fstream infile;
+    infile.open (pathname_in, ios::in | ios::binary);
+    if (!infile.is_open ()) {
         cerr << "Couldn't find file '" << pathname_in << "'." << endl;
-	    exit (-1);
-   }
-   vimage rom (infile, offset);
-   vsegment seg (&rom, cpu);
-   infile.close ();
+        exit (-1);
+    }
+    vimage rom (infile, offset);
+    vsegment seg (&rom, cpu);
+    infile.close ();
 
-   // Process configuration file for image.
-   config_read (seg, cin);
+    // Process configuration file for image.
+    config_read (seg, cin);
 
-   // Trace image.
-   if (no_trace == false)
-      seg.trace ();
+    // Trace image.
+    if (no_trace == false)
+        seg.trace ();
 
-   //if (no_unreached_code == false)
-      //seg.trace_unreached ();
+    //if (no_unreached_code == false)
+       //seg.trace_unreached ();
 
-   // Dump everything to stdout.
-   seg.dump ();
+    // Dump everything to stdout.
+    seg.dump ();
 
-   return 0;
+    return 0;
 }
